426efd10f88e6421be2b66bc1f5e2d8d2968a8cc
Maria Stashenko <mahryia@gmail.com>
2025-10-04 18:11:03 +0300

    DEV-17 add migrations to update bank_accounts

--- Migrations/20251004151022_AddCurrencyScaleToBankAccounts.Designer.cs
+++ Migrations/20251004151022_AddCurrencyScaleToBankAccounts.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using FintechStatsPlatform.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FintechStatsPlatform.Migrations
 {
     [DbContext(typeof(FintechContext))]
-    partial class FintechContextModelSnapshot : ModelSnapshot
+    [Migration("20251004151022_AddCurrencyScaleToBankAccounts")]
+    partial class AddCurrencyScaleToBankAccounts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20251004151022_AddCurrencyScaleToBankAccounts.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FintechStatsPlatform.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCurrencyScaleToBankAccounts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "account_id",
+                table: "users",
+                newName: "account_ids");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "balance",
+                table: "bank_accounts",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(10,2)",
+                oldDefaultValue: 0m);
+
+            migrationBuilder.AddColumn<short>(
+                name: "currency_scale",
+                table: "bank_accounts",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)2);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "currency_scale",
+                table: "bank_accounts");
+
+            migrationBuilder.RenameColumn(
+                name: "account_ids",
+                table: "users",
+                newName: "account_id");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "balance",
+                table: "bank_accounts",
+                type: "numeric(10,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldDefaultValue: 0L);
+        }
+    }
+}
