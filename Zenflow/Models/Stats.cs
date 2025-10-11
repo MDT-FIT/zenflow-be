a32a34b4333ee7b601e853a9011e25bc7744e7ee
Maria Stashenko <mahryia@gmail.com>
2025-10-11 18:05:41 +0300

    DEV-19 update expenses retrieval, stable currency and scale assumption

--- Zenflow/Models/Stats.cs
+++ Zenflow/Models/Stats.cs
@@ -2,13 +2,14 @@
 {
     public class Stats : AbstractEntity
     {
-        public Stats(string userId, List<string>? accountIds = null, long amount = 0, int scale = 2, string currency = "")
+        public Stats(string userId, List<string>? accountIds = null, long amount = 0, int scale = 2, string currency = "", double changePercentage = 0.0)
         {
             UserId = userId;
             AccountIds = accountIds ?? new List<string>();
             Amount = amount;
             Scale = scale;
             Currency = currency;
+            ChangePercentage = changePercentage;
         }
         public string UserId { get; set; }
 
@@ -19,5 +20,7 @@
         public int Scale { get; set; }
 
         public string Currency { get; set; }
+        
+        public double ChangePercentage { get; set; }
     }
 }
--- Zenflow/Services/AnalyticService.cs
+++ Zenflow/Services/AnalyticService.cs
@@ -2,13 +2,14 @@
 using FintechStatsPlatform.Filters;
 using FintechStatsPlatform.Models;
 using Mapster;
+using Microsoft.IdentityModel.Protocols.Configuration;
 
 namespace FintechStatsPlatform.Services
 {
     public class AnalyticService
     {
         private BankService _bankService;
-        
+
         public AnalyticService(BankService bankService)
         {
             _bankService = bankService;
@@ -16,23 +17,65 @@ namespace FintechStatsPlatform.Services
 
         public async Task<Stats> getExpensesAsync(StatsFilter filter, string userAccessToken)
         {
-            var transactionsFilter = filter.ToTransactionFilter(); // Could use AutoMapper as well
+            // Prepare filters for different periods of time
+            var tFilter1 = filter.ToTransactionFilter();
+            var tFilter2 = filter.ToTransactionFilter();
 
-            var transactions = await _bankService.ListTransactionsAsync(transactionsFilter, userAccessToken);
+            DateTime lastMonthDateTo = filter.DateFrom.AddDays(-1);
+            DateTime lastMonthDateFrom = lastMonthDateTo.AddDays(-30);
 
-            var expenses = transactions.Where(t => t.Amount < 0).ToArray();
+            tFilter2.DateFrom = lastMonthDateFrom;
+            tFilter2.DateTo = lastMonthDateTo;
 
-            long totalAmount = expenses.Sum(t => t.Amount);
+            // Get transactions for the current period and 30-days-period before
+            var currentExpensesRetrieval = _bankService.ListTransactionsAsync(tFilter1, userAccessToken);
+            var lastMonthExpensesRetrieval = _bankService.ListTransactionsAsync(tFilter2, userAccessToken);
+
+            await Task.WhenAll(currentExpensesRetrieval, lastMonthExpensesRetrieval); // Wait for both operations to complete
+
+            // Get the resulting transactions
+            var currentExpenses = currentExpensesRetrieval.Result.Where(t => t.Amount < 0).ToArray();
+            var lastMonthExpenses = lastMonthExpensesRetrieval.Result.Where(t => t.Amount < 0).ToArray();
+
+            long currentAmount = Math.Abs(currentExpenses.Sum(t => t.Amount));
+            long lastMonthAmount = Math.Abs(lastMonthExpenses.Sum(t => t.Amount));
+
             int scale = 0;
             string currency = string.Empty;
 
-            if (expenses.Length > 0)
+            // Assume for now that scale and currency are the same throughout all transactions 
+            if (currentExpenses.Length > 0)
             {
-                scale = expenses[0].Scale;
-                currency = expenses[0].Currency;
+                scale = currentExpenses[0].Scale;
+                currency = currentExpenses[0].Currency;
+            }
+            else if (lastMonthExpenses.Length > 0)
+            {
+                scale = lastMonthExpenses[0].Scale;
+                currency = lastMonthExpenses[0].Currency;
             }
 
-            return new Stats(filter.UserId, [.. filter.AccountIds], totalAmount, scale, currency);
+            double changePercentage = CalculateChangePercentage(currentAmount, lastMonthAmount);
+
+            return new Stats(filter.UserId, [.. filter.AccountIds], currentAmount, scale, currency, changePercentage);
+        }
+
+        private double CalculateChangePercentage(long current, long prev)
+        {
+            if (current == 0 && prev == 0)
+            {
+                return 0;
+            }
+            else if (current == 0)
+            {
+                return -100;
+            }
+            else if (prev == 0)
+            {
+                return 100;
+            }
+
+            return (double)(current - prev) / prev * 100;
         }
 
         public Stats getIncome(StatsFilter filter)
