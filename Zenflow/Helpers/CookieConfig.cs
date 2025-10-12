6acbf750d2a6ce322c4ee6a9d66e1a32e3dbd460
Maria Stashenko <mahryia@gmail.com>
2025-10-12 11:12:04 +0300

    DEV-19 add cookie default config

---
+++ Zenflow/Helpers/CookieConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace FintechStatsPlatform.Helpers
+{
+    public static class CookieConfig
+    {
+        public static CookieOptions Default => new()
+        {
+            HttpOnly = true,
+            Secure = true,
+            SameSite = SameSiteMode.Strict,
+            Expires = DateTimeOffset.UtcNow.AddHours(1)
+        };
+    }
+}
