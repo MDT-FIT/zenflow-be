8ec4f0e40e377755878296840567a784dea9ea14
Anna Androschuk [Fukuro] <anya.androschuk@gmail.com>
2025-10-29 19:49:10 +0200

    feat: enable swagger for prod auto-gen FE endpoints

--- Zenflow/Program.cs
+++ Zenflow/Program.cs
@@ -114,13 +114,11 @@ namespace FintechStatsPlatform
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline
-            if (app.Environment.IsDevelopment())
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             app.UseHttpsRedirection();
 
             app.UseCors("AllowAll");
